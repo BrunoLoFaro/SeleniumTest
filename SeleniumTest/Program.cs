644a04e96ecd28d191a6691d4dc411b206c1b6e1
BrunoLoFaro <lofarobruno@gmail.com>
2024-02-16 12:55:23 -0300

    initial commit

---
+++ SeleniumTest/Program.cs
@@ -0,0 +1,7 @@
+﻿using SeleniumTest;
+
+Scraper scraper = new Scraper();
+
+Console.WriteLine("Hello, World!");
+scraper.Navigate();
+Console.WriteLine("ending program");
---
+++ SeleniumTest/Scraper.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumTest
+{
+    internal class Scraper
+    {
+        private IWebDriver _webDriver;
+        public Scraper()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            _webDriver = new ChromeDriver();
+        }
+        public void Navigate()
+        {
+            _webDriver.Navigate().GoToUrl("https:www.github.com");
+        }
+    }
+}
